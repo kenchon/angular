e8b5c00b7f778506af82fca4c54ac098a8149b54
kenmikanmi <kenh5526@gmail.com>
2019-09-04 10:14:13 +0900

    MODIFIED to play with event processing

--- prop/src/app/app.component.ts
+++ prop/src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<div>{{msg}}</div>`
+  template: `
+    <input type="button" (click)="back=!back;fore=!fore" value="Background"/>
+    <div [ngStyle]="styles">
+    {{msg}}
+    </div>`
   //ブラケット構文
   //template: `<img [src]="image" />`
 
@@ -13,6 +17,26 @@ import { Component } from '@angular/core';
   //template: `<img src="{{image}}" />`  
 })
 export class AppComponent {
-  msg: string = `Hello World!`
+  msg: string = `Hello World!`;
+  style = {
+    backgroundColor: '#12346b',
+    color: '#fff',
+    fontWeight: 'bold',
+    margin: '15px',
+    padding: '15px'
+  };
+  back = false;
+  fore = false;
+
+  get styles(){
+    return {
+      'background-color': this.back ? '#12346b': '',
+      'color': this.fore ? '#fff': '',
+      fontWeight: 'bold',
+      margin: '15px',
+      padding: '15px'
+    };
+  };
+
   //image = 'http://www.wings.msn.to/image/wings.jpg';
 }
